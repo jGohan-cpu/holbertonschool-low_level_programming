2cb1b72821c6ff458bf0ad08955720c367332ea8
jGohan-cpu <jjoseriveram01@gmail.com>
2022-10-05 22:58:50 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
-/*
- * File: 0-holberton.c
- */
-
 #include "main.h"
-
 /**
+ * main -Prints
  *
- * Return: Always 0.
+ * Return: Prints a file called _putchar
  */
 int main(void)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+        putchar(' ');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
