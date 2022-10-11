f092abe025de4255945c17595fb6e0aef398ed0f
jGohan-cpu <jjoseriveram01@gmail.com>
2022-10-11 18:45:25 +0000

    5th exercise

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-  * @n: Number that gets checked
-  *
   * print_sign - Prints the sign of a number
+  * @n: The number to be checked
   *
   * Return: 1 for positive num, -1 for negative num or zero for anything else
   */
