39b10d112c28733af2176126ac5daef9b396c1a1
jGohan-cpu <jjoseriveram01@gmail.com>
2022-10-12 02:38:34 +0000

    2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
-  * mul - Multiplies two integers
-  * @x: The first number to multiply
-  * @y: The second number to multiply with first number
-  *
-  * Return: The result of multiplication between x param and y param
-  */
-int mul(int x, int y)
+ * mul - function that prints the numbers, from 0 to 9, followed by a new line.
+ * @a: the int for the paramaters of my function
+ * @b: the int for the paramaters of my function
+ * Return: 0
+ */
+int mul(int a, int b)
 {
-	return (x * y);
+	return (a * b);
 }
