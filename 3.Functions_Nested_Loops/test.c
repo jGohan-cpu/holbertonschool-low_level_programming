22fcb60dc26c3fbfc15d6aeda38870832ebfda64
jGohan-cpu <jjoseriveram01@gmail.com>
2022-10-17 14:48:23 +0000

    5

---
+++ 3.Functions_Nested_Loops/test.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+int main(void)
+{
+
+int AA;
+
+for (AA ='0'; AA <='9'; AA ++)
+{
+
+   for (AA ='0'; AA <='9'; AA ++)
+
+{
+   printf("Test\n");
+}
+}
+}
+return (0);
--- 5.More_Functions_Nested_Loops/5-more_numbers.c
+++ 5.More_Functions_Nested_Loops/5-more_numbers.c
@@ -9,16 +9,11 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y > 9)
-			{
-				_putchar((y / 10) + '0');
-			}
-
-			_putchar((y % 10) + '0');
+			_putchar(y);
 		}
 
 		_putchar('\n');
