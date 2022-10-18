bb655cd1c30460e6eac434208d42cc68e43d9fdc
jGohan-cpu <jjoseriveram01@gmail.com>
2022-10-18 21:09:21 +0000

    0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,21 +6,14 @@
 
 void _puts_recursion(char *s)
 {
-
-int lenght = strlen(string);
-int middle = lenght / 2;
-char temp;
-
-
-for(i=0; i < middle; i++)
-     {
-      
-      temp=string[i];
-      string[i] = string[length - i - 1];
-      string[lenght - i - 1] = temp; 	      	      
-
-     } 
-
-
+if(*s != '\0')
+           {
+            _putchar(*s);
+	    _puts_recursion(s + 1);
+           }
+else
+	   {
+	_putchar('\n');
+           }
 
 }
