75252d543ab36b6e2e188034ab6d0ed24d01eda3
jGohan-cpu <jjoseriveram01@gmail.com>
2022-10-19 19:15:16 +0000

    1

--- 3.Functions_Nested_Loops/7-print_last_digit.c
+++ 3.Functions_Nested_Loops/7-print_last_digit.c
@@ -8,16 +8,7 @@
   */
 int print_last_digit(int n)
 {
-	int last;
+	int last = n % 10;
 
-	last = n % 10;
-
-	if (last < 0)
-	{
-		last = last * -1;
-	}
-
-	_putchar(last + '0');
-
-	return (last);
+	
 }
